b14a10e351d248197ff91adabfad3761f2ea64a0
Pranchipandey <pranchi.pandey_cs19@gla.ac.in>
2022-03-21 11:34:01 +0530

    Time: 3 ms (33.38%), Space: 54.9 MB (31.98%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n =nums.length;
+        int j=0;
+        for(int i=0; i<n;i++){
+            if(nums[i]!=0 && nums[j]==0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+            if(nums[j]!=0){
+                j++;
+            }
+        }
+    }
+}
