c7e283abea989568cfaef80c55510640b8e9db19
Pranchipandey <pranchi.pandey_cs19@gla.ac.in>
2022-06-24 13:26:00 +0530

    Added solution - LeetHub

---
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.java
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	 
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int t = Integer.parseInt(br.readLine()); 
+
+        while(t-- > 0){
+            int size = Integer.parseInt(br.readLine());
+            String[] arrStr = ((String)br.readLine()).split("\\s+");
+            int[] arr= new int[size];
+            for(int i = 0;i<size;i++){
+                arr[i] = Integer.parseInt(arrStr[i]);
+            }
+            System.out.println(new Solution().minJumps(arr));
+        }
+	 }
+	 
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int minJumps(int[] arr){
+        int n = arr.length;
+         if(n <= 1) { return 0; }
+        if(arr[0] == 0) { return -1; }
+        
+        int maxReach = arr[0];
+        int step = arr[0];
+        int jump = 1;
+        
+        int i = 1;
+        for(i = 1; i<n; i++) {
+            if( i == n-1) { return jump; }
+            
+            maxReach = Math.max(maxReach, i + arr[i]);
+            step--;
+            
+            if(step == 0){
+                jump++;
+                if(i >= maxReach) { return -1; }
+                step = maxReach - i;
+            }
+        }
+        return -1;
+    }
+}
