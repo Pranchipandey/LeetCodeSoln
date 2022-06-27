508e76bce9989ec2c122308d10786514178bc624
Pranchipandey <pranchi.pandey_cs19@gla.ac.in>
2022-06-27 16:48:20 +0530

    Time: 2 ms (64.84%), Space: 43.3 MB (82.18%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+      ArrayList<Integer> l= new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                l.add(0);
+                l.add(0);
+            }
+            else{
+                l.add(arr[i]);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=l.get(i);
+        }
+    }
+}
