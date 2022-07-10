e3883bae692b57dcc93027b0a1f70d120521ccb6
Pranchipandey <pranchi.pandey_cs19@gla.ac.in>
2022-07-10 23:27:57 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (77.63%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,14 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+       ListNode slow= head;
         ListNode fast = head;
-        ListNode slow = head;
         while(fast!=null && fast.next!=null){
             fast=fast.next.next;
-            slow=slow.next;
-            if(slow==fast){
+            slow= slow.next;
+            if(fast==slow){
                 return true;
             }
+            
         }
         return false;
     }
